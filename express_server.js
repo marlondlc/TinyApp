ce5e84bce25fc666065411192712d60c1cebf2d8
marlondlc <m.de.la.cruz@hotmail.com>
2019-01-22 00:48:18 +0000

    another big commit

--- express_server.js
+++ express_server.js
@@ -1,147 +1,327 @@
+///__________________________FrameWork __________________________________________________________________________________________//
+// express adds packages to build wed applications
 
-///__________________________FrameWork _______________________________//
-// express adds packages to build wed applications 
-
-const express = require( "express" );
+const express = require("express");
 const app = express(); // this means the application is running with express NPM
-const PORT = 8080; // default port 8080
-const uuidv1 = require( "uuid/v1" ) //feature to gen random number
+const PORT = 3000; // default port 8080
+const uuidv1 = require("uuid/v1"); //feature to gen random number
+const cookieParser = require("cookie-parser");
 
-//__________________________MiddleWare_____________________________//
+//__________________________MiddleWare__________________________________________________________________________________________//
 //middle ware definition = define app endpoints(uri's)//sends response back to the client // matches request routes using pattern expressions.
-//bodyParser - allowing you to extract 
+//bodyParser - allowing you to extract
 //set "View Engine" = set what kind of templete file to use (ex. html/ejs) this ex uses ejs
-// MW - gives you an additon feature 
-const bodyParser = require( "body-parser" ); // installed npm body-parser and linked it
-app.use( bodyParser.urlencoded( { extended: true } ) );// methodes
-app.set( "view engine", "ejs" ); // method
+// MW - gives you an additon feature
+const bodyParser = require("body-parser"); // installed npm body-parser and linked it
+app.use(bodyParser.urlencoded({ extended: true })); //
+app.set("view engine", "ejs"); // ejs engine
+app.use(cookieParser());
 
-
-//__________________DataBase Obj (Dom used MovieQuoteDB)______________________//
+//__________________DataBase Object __________________________________________________________________________________________//
 //This is where you are pulling info (check with mentor)
 // When sending variables to an EJS template, you need to send them inside an object, even if you are only sending one variable
-
+//URL DATABASE
 var urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com",
+  b2xVn2: {
+    shortUrl: "b2xVn2",
+    longUrl: "http://www.lighthouselabs.ca",
+    userId: "userRandomID"
+  },
+
+  "9sm5xK": {
+    shortUrl: "9sm5xK",
+    longUrl: "http://www.google.com",
+    userId: "user2RandomID"
+  }
+};
+//USERs DATABASE // task 3 w2d4 COMPASS
+const usersDB = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  //-Added as an example: Compass  asked to add one and see if you can login with it which you could
+  user3RandomID: {
+    id: "user3RandomID",
+    email: "marlon@example.com",
+    password: "123abc"
+  }
 };
 
-
-//________________Functions-Below______________________________//
+//______________________________________Functions-Below___________________________________________________________________________//
 
 //below function as indicated will give you a random string of 6 random alpha characters
-function generateRandomString ()
-{
-    //1st step - new to create an id: // short URL
-    return uuidv1().substring( 0, 6 );
-    //console.log( "ID", id )
-
-
+function generateRandomString() {
+  //1st step - new to create an id: // short URL
+  return uuidv1().substring(0, 6);
+  //console.log( "ID", id )
 }
 
+//--------------------1 Function-----Create Random User-----------------------------------//
+const createUser = (email, password) => {
+  // geerate a random id
+  const userId = generateRandomString(); // function
 
+  //create new user obj {
+  //id: "user2RandomID",
+  // email: "user2@example.com",
+  //password: "dishwasher-funk"
+  // }
+  const newUser = {
+    id: userId,
+    email: email,
+    password: password
+  };
+  // add a new user obj to the user db
+  usersDB[userId] = newUser;
+  console.log("users:", usersDB);
+  return userId;
+};
 
+//-------------------------2 Function----emailExist----------------------------------//
 
+//or const emailExist = email => {}
+const emailExist = email => {
+  //loop through the existing user obj
+  for (const userId in usersDB) {
+    //if the email is the same then i got the right user
+    //then return the userId
+    if (usersDB[userId].email === email) {
+      return userId;
+    }
+  }
+  return false;
+  //maybe  later on i might change this function to check PW as well..we will see!
+};
 
+//________________________________3rd Function  -- urls for users_________________________________________________//
 
-//_____________________________________________________________//
+function urlForUsers(id) {
+  const filteredUrl = {};
 
+  for (const shortUrl in urlDatabase) {
+    const urlObj = urlDatabase[shortUrl];
+    if (urlObj.userId === id) {
+      filteredUrl[shortUrl] = urlObj;
+    }
+  }
+  return filteredUrl;
+}
 
-
-
-//____________________________END POINTS - SENDING DATA "GET"________________________________________//
+//__________________________________END POINTS - SHOW THE WORK "GET"__________________________________________________________________//
 //Get =  route to show the form
 //Render = "res.render" will send the OBJ we created to the EJS file (ex. "urls_index")
 // struct = get / post / update / delete
-app.get( "/urls", ( req, res ) =>
-{
-    let templateVars = { urls: urlDatabase };
-    res.render( "urls_index", templateVars );
-
-
-} );
+app.get("/urls", (req, res) => {
+  const userId = req.cookies["user_id"];
+  //added the line above // this is what was there before let user = usersDB[req.cookies.user_id];
+  let templateVars = {
+    urls: urlForUsers(userId),
+    user: usersDB[req.cookies.user_id]
+  }; //2nd user is reffering to previous line
+  console.log(templateVars.user);
+  res.render("urls_index", templateVars);
+});
 //adding a GET route to show form
-app.get( "/urls/new", ( req, res ) =>// give me the url_new (render below)
-{
-    res.render( "/urls_new" );
+app.get("/urls/new", (req, res) => {
+  let user = usersDB[req.cookies.user_id];
+  let templateVars = { user: req.cookies["user_id"] };
 
-} );
+  // give me the url_new (render below)
+  res.render("urls_new", templateVars);
+});
 
-app.get( "/urls/:id", ( req, res ) =>
-{
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {
+    longUrl: urlDatabase[req.params.id],
+    //long url is trying to access  the [] in this ex. "http://www.lighthouselabs.ca"
+    //[] = OBJECT [key] returns value- "http://...""
+    shortUrl: req.params.id,
+    user: req.cookies["user_id"]
+  };
+  //[ req.params.id ] -- gives you back the id within the obj indicated before
+  //console.log( urlDatabase[req.params.id ] )
+  res.render("urls_show", templateVars);
+  console.log(templateVars);
+});
+//login --------> get------> show the login page
+//logout -------->get------> show the logout page
+//register ------>get------> show me the register page
+//------End point for managing the login
 
-    let templateVars = {
-        longUrl: urlDatabase[ req.params.id ],
-        //long url is trying to access  the [] in this ex. "http://www.lighthouselabs.ca" 
-        //[] = OBJECT [key] returns value- "http://...""
+app.get("/login", (req, res) => {
+  //render to login
+  res.render("url_login");
+});
 
-        shortUrl: req.params.id
-    };
-    //[ req.params.id ] -- gives you back the id within the obj indicated before
-    //console.log( urlDatabase[ req.params.id ] )
-    res.render( "urls_show", templateVars );
-    console.log( templateVars );
-} );
+//-----------register endpoint ----------
 
-app.get( "/", ( req, res ) =>
-{
-    res.send( "Hello!" );
-} );
+app.get("/register", (req, res) => {
+  // add let templateVar = {user:null} this could work instead of the type of on ejs "header"
+  res.render("register");
+  //NEED TO ADD SOMETHING HERE FROM VIDEO // line 186/ line
+});
 
-app.get( "/urls.json", ( req, res ) =>
-{
-    res.json( urlDatabase );
-} );
-app.get( "/hello", ( req, res ) =>
-{
-    res.send( "<html><body>Hello <b>World</b></body></html>\n" );
-} );
+//____________________________________________________________________________________//
+//____________________________________________________________________________________//
+//____________________________________________________________________________________//
 
-app.post( "/urls", ( req, res ) =>
-{
-    let rNumber = generateRandomString();
-    //console.log( req.body );  // debug statement to see POST parameters
-    urlDatabase[ rNumber ] = req.body.longURL;
-    console.log( urlDatabase )
-    res.redirect( '/urls' ); // redirect you back to the home page. (ex "/url = home page ")
-} );
+/*NOT IN USE RIGHT NOW
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
-app.post( "/urls/:id/delete", ( req, res ) =>
-{
-    delete urlDatabase[ req.params.id ] //
-    res.redirect( '/urls' ); // redirect you back to the home page. (ex "/url = home page ")
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+*/
 
-} );
+//____________________________POST END POINTS (add/delete/update)(ALL THE POSTS DO THE WORK)_____________________________________//
 
-app.post( '/urls/:id/update', ( req, res ) =>
-{
-    let templateVars = { longURL: urlDatabase[ req.params.id ], shortURL: req.params.id };
-    res.render( "shortUrl", templateVars )
-} )
-// app.post( '/urls/:id/edit', ( req, res ) =>
+app.post("/urls", (req, res) => {
+  let rNumber = generateRandomString();
+
+  //console.log( req.body );  // debug statement to see POST parameters
+  urlDatabase[rNumber] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect("/urls"); // redirect you back to the home page. (ex "/url = home page ")
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  // Routing To Server -ex
+  delete urlDatabase[req.params.id]; //
+  res.redirect("/urls"); // redirect you back to the home page. (ex "/url = home page ")
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  //
+  //this function all it does is update the link "update"//
+  //extraxt info from url ID
+  const shortUrl = req.params.id;
+  // extract info from the form being submitted
+  const longUrl = req.body.longURLChangedName;
+  //update the url in your URL-DB then redirect
+  urlDatabase[shortUrl] = longUrl;
+  console.log("longUrl", longUrl);
+  console.log("re.boy", req.body);
+  //its going to find the obj URL DB and replace its value by (longUrl)
+  //redirect
+  res.redirect("/urls");
+  //THIS TASK IS DOING BELOW:
+  // This portion of the assignment requires changes on the client and the server.
+  // Once the user submits an Update request, it should modify the corresponding
+  // longURL, and then redirect the client back to "/urls".
+});
+
+app.post("/register", (req, res) => {
+  //task 4 -- w2d4 COMPASS (user registration handler)
+  // add user to database/ tag you /redirect to hompage)
+  const email = req.body.email;
+  const password = req.body.password;
+  const email_password_empty = !email || !password;
+  console.log("email_password_empty:", email_password_empty); //output should be be an empty
+
+  // If the e-mail or password are empty strings, send back a response with the 400 status code.
+  // If someone tries to register with an existing user's email, send back a response with the 400 status code.
+  console.log("find user: ");
+
+  if (email_password_empty) {
+    // Task 5 w2d4 COMPASS
+    res.status(400).send("please fill out the required field");
+  } else if (emailExist(email)) {
+    res.status(400).send("User already exist. Please Login!");
+  } else {
+    console.log("req.body", req.body);
+    console.log("email:", email, "password:", password);
+    const userId = createUser(email, password);
+    console.log(createUser);
+    //set the cookie with the userId
+    res.cookie("user_id", userId);
+    // res.redirect to /urls
+    res.redirect("/urls");
+    // check the brower via dev mode under application to see if this works
+    console.log(userId);
+  }
+});
+
+app.post("/login", (req, res) => {
+  //this line verifys credentials reroute to hompage and tag you "stamp" (if valid login).
+  // It should set a cookie named username to the value submitted in the request body via the login form.
+  //const userName = req.body.email;
+  const email = req.body.email;
+  const passwordForm = req.body.password;
+  const email_password_empty = !email || !passwordForm;
+  const userIdDB = emailExist(email);
+  console.log(req.body.email);
+  if (email_password_empty) {
+    // Task 5 w2d4 COMPASS
+    res.status(400).send("Please fill out the required feild");
+  } else if (!userIdDB) {
+    // if no email please redirect to error msg
+    res.status(400).send("wrong credentials!");
+  } else {
+    //now that we know email exist you need to compare PW
+    if (usersDB[userIdDB].password === passwordForm) {
+      // or req.body.password:
+      res.cookie("user_id", userIdDB);
+      // res.redirect to /urls
+      res.redirect("/urls"); //After your server has set the cookie it should redirect the browser back to the /urls page.
+
+      // check the brower via dev mode under application to see if this works
+    } else {
+      res.status(400).send("wrong credentials!");
+    }
+  }
+});
+
+app.post("/logout", (req, res) => {
+  // it will wash your stamp off (remove the stamp) //TASK7-W2D2
+  //remove the cookie from the user ID
+  res.clearCookie("user_id");
+  res.redirect("/urls");
+});
+
+//_______________________________________________________________________________________________________________//
+
+// 1 EXAmPLE-
+// app.post( "/edit", ( req, res ) =>
 // {
-//     let templateVars = { longURL: urlDatabase[ req.params.id ], shortURL: req.params.id };
-//     res.direct( "longUrl", templateVars )
+//     UserName = req.cookies;
+//     //console.log( req.body );  // debug statement to see POST parameters
+//     urlDatabase[ rNumber ] = req.body.longURL;
+//     console.log( urlDatabase )
+//     res.redirect( '/urls' ); // redirect you back to the home page. (ex "/url = home page ")
+// } );
 
+// app.get( '/login', function ( req, res )
+// {
+//     const userId = req.cookie.userId
+//     const currentUser = urlDatabase[ userId ]
 
 // } )
 
-app.listen( PORT, () =>
-{
-    console.log( `Example app listening on port ${ PORT }!` );
-}
-);
+//____________________________________________________________________________________//
+//____________________________________________________________________________________//
 
-//________________________________________________________________________________//
-
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 // post request --
 
 //_______________________QUESTIONS____________________________________//
 //In Express, when an EJS template is rendered using res.render(), the EJS is evaluated on the server or client?
 //ANSWER: On the server, before the HTTP response gets sent to the client
-//________although possible to render client-side templates, we use it as a 
+//________although possible to render client-side templates, we use it as a
 //________server-side technology. ALSO /Either server or client, but it's predominantly found server-side
 //________although possible to render client - side templates with EJS, that is not how we will be using it
 
@@ -175,3 +355,9 @@ app.listen( PORT, () =>
 // names.push('Jordan');
 // console.log( names );
 //Console would show ['Jordan'] -it still allows us to change the value of the referenced variable
+
+//TASK 6//
+
+//same principle for the rest
+
+//curl -X POST "http://localhost:8080/urls/9sm5xK/delete"
